465989c72d9078f1ac42ab84b9fae9b316fe9f2f
Raushan roy <raushanroyece062@gmail.com>
2022-02-20 12:39:21 +0530

    try catch basics

---
+++ Basic.java
@@ -0,0 +1,19 @@
+class Basic{
+    public static void main(String[] args) {
+
+        //abnormal termination of program
+        System.out.println("statement -1");
+        System.out.println(10/0);
+        System.out.println("statement - 1");
+        
+        //normal termination of program
+        // System.out.println("statement -1");
+        // try{
+        //      System.out.println(10/0);
+        // }
+        // catch(ArithmeticException e){
+        //     System.out.println(10/2);
+        // }
+        // System.out.println("statement -2");
+    }
+}
